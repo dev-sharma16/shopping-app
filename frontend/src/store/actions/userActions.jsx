703a97b49d5b300d6a4f7ad641108aaa9104b31a
Dev Sharma <devsharma1683@gmail.com>
2025-06-29 21:12:14 +0530

    action/feature file for user (regUser,loginUser,logout,currentUser)

---
+++ frontend/src/store/actions/userActions.jsx
@@ -0,0 +1,43 @@
+import axios from '../../api/axiosConfig'
+import { loadUser } from '../reducers/userSlice';
+
+export const asyncRegisterUser = (user) => async(dispatch, getState) => {
+    try {
+        const res = await axios.post('/users', user);
+        console.log(res);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const asyncLoginUser = (user) => async(dispatch, getState) => {
+    try {
+        const {data} = await axios.get(`/users?email=${user.email}&password=${user.password}`);
+        localStorage.setItem("user", JSON.stringify(data[0]))
+        console.log(data[0]);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const asyncLogOutUser = () => async(dispatch, getState) => {
+    try {
+        localStorage.removeItem("user");
+        console.log("User logged out..!");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const asyncCurrentUser = () => async(dispatch, getState) => {
+    try {
+        const user = JSON.parse(localStorage.getItem("user"));
+        if(user){
+            dispatch(loadUser(user))
+        } else{
+            console.log("User not found..!");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
