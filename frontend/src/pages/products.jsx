83075ae5396491e991e6e0b2ed467670e4af24d9
Dev Sharma <devsharma1683@gmail.com>
2025-06-30 13:37:32 +0530

    product listing page

--- frontend/src/pages/products.jsx
+++ frontend/src/pages/products.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import ProductCard from '../components/ProductCard'
+import { useNavigate } from 'react-router-dom'
 
 export const Products = () => {
+  const products = useSelector((state)=> state.products.data)
+  console.log(products);
+  
+  const navigate = useNavigate()
+
+  const handleClick = (product) => {
+    navigate(`/product/${product.id}`);
+  }
+
   return (
-    <div>Products</div>
+    <div className='h-full w-full bg-gray-800'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {products.map((product, index) => (
+          <ProductCard
+            key={index}
+            imageUrl={product.imageUrl}
+            title={product.title}
+            price={product.price}
+            category={product.category}
+            onClick={()=>handleClick(product)}
+          />
+        ))}
+      </div>      
+    </div>
   )
 }
