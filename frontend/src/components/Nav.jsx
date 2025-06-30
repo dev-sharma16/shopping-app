c0552f1c8bbf5b1f91868c8f8715c7a0424b6ec9
Dev Sharma <devsharma1683@gmail.com>
2025-06-30 13:35:56 +0530

    added logout func

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { asyncLogOutUser } from '../store/actions/userActions'
+
 export const Nav = () => {
     const user = useSelector((state)=> state.user.data)
     console.log(user);
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const LougOutHandler = () => {
+        dispatch(asyncLogOutUser());
+        navigate("/login");
+    }
     
     return (
         <nav className='mb-10 flex justify-center items-center gap-x-5 p-10'>
-            {user ? (
+            {user? (
                 <>
                     <NavLink to='/'>Home</NavLink>
                     <NavLink to='/products'>Products</NavLink>
-                    <NavLink to='/admin/create-product'>Create Product</NavLink>
-                    <NavLink to='/home'>Logout</NavLink>
+                    {user.isAdmin ? <NavLink to='/admin/create-product'>Create Product</NavLink> : ""}
+                    <button 
+                        onClick={LougOutHandler}
+                        className="transition-all duration-150 active:scale-95 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50 px-2 py-1 rounded"
+                    >
+                        LogOut
+                    </button>
                 </>
             ) : (
                 <>
