aadd690dd8323c6250e1c7140ad0f688460ba89d
Dev Sharma <devsharma1683@gmail.com>
2025-07-01 13:43:44 +0530

    removed logout button and add a different section for user profile

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -6,31 +6,20 @@ import { asyncLogOutUser } from '../store/actions/userActions'
 export const Nav = () => {
     const user = useSelector((state)=> state.user.data)
     console.log(user);
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const LougOutHandler = () => {
-        dispatch(asyncLogOutUser());
-        navigate("/login");
-    }
     
     return (
         <nav className='mb-10 flex justify-center items-center gap-x-5 p-10'>
             {user? (
                 <>
                     <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/products'>Products</NavLink>
+                    {/* <NavLink to='/products'>Products</NavLink> */}
                     {user.isAdmin ? <NavLink to='/admin/create-product'>Create Product</NavLink> : ""}
-                    <button 
-                        onClick={LougOutHandler}
-                        className="transition-all duration-150 active:scale-95 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50 px-2 py-1 rounded"
-                    >
-                        LogOut
-                    </button>
+                    <NavLink to={'/profile/'+user.id}>Profile</NavLink>
                 </>
             ) : (
                 <>
                     <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/products'>Products</NavLink>
+                    {/* <NavLink to='/products'>Products</NavLink> */}
                     <NavLink to='/login'>Login</NavLink>
                 </>
             )}
