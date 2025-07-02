812f23af7a3ec139f694cfdd2db2588ba374d933
Dev Sharma <devsharma1683@gmail.com>
2025-07-02 13:25:45 +0530

    add a path for profile and cart

--- frontend/src/routes/Mainroutes.jsx
+++ frontend/src/routes/Mainroutes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
-import { Products } from '../pages/Products'
+import Products from '../pages/products'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 import CreateProduct from '../pages/admin/CreateProduct'
@@ -10,10 +10,11 @@ import { useSelector } from 'react-redux'
 import UserProfile from '../pages/user/UserProfile'
 import PageNotFound from '../pages/PageNotFound'
 import AuthWrapper from './AuthWrapper'
+import Cart from '../pages/Cart'
 
 export const Mainroutes = () => {
     const user = useSelector((state)=> state.user.data)
-    console.log(user);
+    // console.log(user);
     
     return (
         <Routes>
@@ -23,17 +24,23 @@ export const Mainroutes = () => {
             <Route path='/login' element={<Login/>}></Route>
             <Route path='/register' element={<Register/>}></Route>
             <Route path='/admin/create-product' element={
-                <AuthWrapper>
+                // ! Auth wraper does not working fix it
+                // <AuthWrapper>
                     <CreateProduct/>
-                </AuthWrapper>
+                // </AuthWrapper>
             }></Route>
             <Route path='/product/:id' element={
-                <AuthWrapper>
+                // <AuthWrapper>
                     <DetailsProducts/>
-                </AuthWrapper>
+                // </AuthWrapper>
             }></Route>
+            <Route path='/cart' element={
+                // <AuthWrapper>
+                    <Cart/>
+                // </AuthWrapper>
+            }></Route>
+            <Route path='/profile/:id' element={<UserProfile/>}></Route>
             <Route path='*' element={<PageNotFound/>}></Route>
-            
         </Routes>
     )
 }
