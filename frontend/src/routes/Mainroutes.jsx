add38b7728cead6fd0ca76e59e244bd4179e0c97
Dev Sharma <devsharma1683@gmail.com>
2025-07-03 12:16:49 +0530

    added lazy loading on all the components and added protection on the routes

--- frontend/src/routes/Mainroutes.jsx
+++ frontend/src/routes/Mainroutes.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
-import Products from '../pages/products'
-import Login from '../pages/Login'
-import Register from '../pages/Register'
-import CreateProduct from '../pages/admin/CreateProduct'
-import DetailsProducts from '../pages/admin/DetailsProducts'
+const Products = lazy(()=> import('../pages/products')) 
+const Login = lazy(()=> import('../pages/Login')) 
+const Register = lazy(()=> import('../pages/Register')) 
+const CreateProduct = lazy(()=> import('../pages/admin/CreateProduct'))
+const DetailsProducts = lazy(()=> import('../pages/admin/DetailsProducts')) 
 import { useSelector } from 'react-redux'
-import UserProfile from '../pages/user/UserProfile'
-import PageNotFound from '../pages/PageNotFound'
-import AuthWrapper from './AuthWrapper'
-import Cart from '../pages/Cart'
+const UserProfile = lazy(()=> import('../pages/user/UserProfile')) 
+const PageNotFound = lazy(()=> import('../pages/PageNotFound')) 
+const AuthWrapper = lazy(()=> import('./AuthWrapper')) 
+const Cart = lazy(()=> import('../pages/Cart')) 
+const UnAuthWrapper = lazy(()=> import('../routes/UnAuthWrapper'))
 
 export const Mainroutes = () => {
-    const user = useSelector((state)=> state.user.data)
+    // const user = useSelector((state)=> state.user.data)
     // console.log(user);
     
     return (
@@ -21,23 +22,34 @@ export const Mainroutes = () => {
             {/* <Route path='/' element={user?<Products/>:<Home/>}></Route> */}
             {/* <Route path='/products' element={<Products/>}></Route> */}
             <Route path='/' element={<Products/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/register' element={<Register/>}></Route>
+
+            //? protected routes from logined user
+            <Route path='/login' element={
+                <UnAuthWrapper>
+                    <Login/>
+                </UnAuthWrapper>
+            }></Route>
+            <Route path='/register' element={
+                <UnAuthWrapper>
+                    <Register/>
+                </UnAuthWrapper>
+            }></Route>
+
+            //? protected routes from non-logined user
             <Route path='/admin/create-product' element={
-                // ! Auth wraper does not working fix it
-                // <AuthWrapper>
+                <AuthWrapper>
                     <CreateProduct/>
-                // </AuthWrapper>
+                </AuthWrapper>
             }></Route>
             <Route path='/product/:id' element={
-                // <AuthWrapper>
+                <AuthWrapper>
                     <DetailsProducts/>
-                // </AuthWrapper>
+                </AuthWrapper>
             }></Route>
             <Route path='/cart' element={
-                // <AuthWrapper>
+                <AuthWrapper>
                     <Cart/>
-                // </AuthWrapper>
+                </AuthWrapper>
             }></Route>
             <Route path='/profile/:id' element={<UserProfile/>}></Route>
             <Route path='*' element={<PageNotFound/>}></Route>
