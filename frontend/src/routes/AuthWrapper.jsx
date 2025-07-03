717161093e72d9038a756e157c6d4d56f48eae61
Dev Sharma <devsharma1683@gmail.com>
2025-07-03 12:16:08 +0530

    wrapper for protection from non logined user

--- frontend/src/routes/AuthWrapper.jsx
+++ frontend/src/routes/AuthWrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 const AuthWrapper = (props) => {
-    const {user} = useSelector((state)=>state.user.data)
+    const user = useSelector((state)=>state.user.data)
 
     return user ? props.children : <Navigate to='/login'/>
 }
